072b3a74329604a6c5cde4821a4e3d1e1b874771
pa001024 <pa001024@qq.com>
2015-11-19 15:34:13 +0800

    update batch

--- vjshi.go
+++ vjshi.go
@@ -11,9 +11,11 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 
+	"github.com/pa001024/reflex/util"
 	"github.com/pa001024/reflex/util/ascgen"
 )
 
@@ -23,20 +25,66 @@ const (
 	DEFAULT_UA       = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"
 )
 
+type UserState struct {
+	name    string
+	success bool
+	bp      int
+}
+
 // VJshi batch op
 func main() {
 	isRegist := flag.Bool("r", false, "isRegist: -r require -u -p -q otherwise -u -p")
 	isBatch := flag.Bool("b", false, "isBatch: -b")
+	prefix := flag.String("prefix", "vjqq12011158", "username prefix")
 	u := flag.String("u", "", "username")
 	p := flag.String("p", DEFAULT_PASSWORD, "password")
 	q := flag.String("q", "", "qq number")
 	flag.Parse()
 	if *isBatch {
+		unamefmt := *prefix + "%02d"
+		key_list := make([]string, 0, 99)
+		key2user := make(map[string]UserState)
 		for i := 1; i <= 99; i++ {
-			uname := fmt.Sprintf("vjqq12011158%02d", i)
+			uname := fmt.Sprintf(unamefmt, i)
+			key_list = append(key_list, uname)
 			v := NewUser(uname, DEFAULT_PASSWORD, "")
-			v.LoginAndSign()
+			err := v.LoginAndSign()
+			bp := 0
+			for i := 0; i < 3; i++ {
+				if err != nil {
+					err = v.LoginAndSign()
+				} else {
+					for j := 0; j < 3; j++ {
+						if bp == 0 {
+							bp, _ = v.GetBounsPoint()
+						}
+					}
+					break
+				}
+			}
+			key2user[uname] = UserState{uname, err == nil, bp}
 		}
+		count_fail, count_bp_all := 0, 0
+		failed_list := ""
+		for _, v := range key_list {
+			if !key2user[v].success {
+				count_fail++
+				failed_list += fmt.Sprintf("vjshi -u %s\n", key2user[v].name)
+			}
+			count_bp_all += key2user[v].bp
+		}
+		if count_fail == 0 {
+			util.INFO.Logf("%s ~ %s 全部签到完毕\n", fmt.Sprintf(unamefmt, 1), fmt.Sprintf(unamefmt, 99))
+		} else {
+			util.INFO.Logf("%s ~ %s 部分签到完毕 失败 %d 个\n", fmt.Sprintf(unamefmt, 1), fmt.Sprintf(unamefmt, 99), count_fail)
+			fmt.Println("输入以下指令重试:")
+			fmt.Println(failed_list)
+		}
+		util.INFO.Logf("今日共获取 %d 积分 总计 %d 积分 平均每账号 %d 积分\n",
+			(99-count_fail)*10,
+			count_bp_all,
+			count_bp_all/99,
+		)
 	} else {
 		if *u == "" {
 			flag.Usage()
@@ -77,7 +125,10 @@ func (this *User) post(api string, param url.Values) (res *http.Response, err er
 	return this.c.Do(req)
 }
 func (this *User) getCapture() (res *http.Response, err error) {
-	req, _ := http.NewRequest("GET", SITE_BASE+"/Base/verify/t/5122223123", nil)
+	return this.get("/Base/verify/t/5122223123")
+}
+func (this *User) get(api string) (res *http.Response, err error) {
+	req, _ := http.NewRequest("GET", SITE_BASE+api, nil)
 	req.Header.Set("User-Agent", DEFAULT_UA)
 	req.Header.Set("Referer", "http://www.vjshi.com/Passport/usercenter/")
 	return this.c.Do(req)
@@ -163,16 +214,28 @@ func (this *User) Sign() (err error) {
 	}
 }
 
-func (this *User) LoginAndSign() {
-	err := this.Login()
+func (this *User) GetBounsPoint() (v int, err error) {
+	res, err := this.get("/Passport/show_headerinfo/")
+	rst, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
+		return
+	}
+	res.Body.Close()
+	ex := regexp.MustCompile(`积分：</span><span class="txt">(\d+)</span>`)
+	ret := ex.FindStringSubmatch(string(rst))
+	if len(ret) > 1 {
+		return strconv.Atoi(ret[1])
+	}
+	return 0, errors.New("未知错误")
+}
+
+func (this *User) LoginAndSign() (err error) {
+	err = this.Login()
+	if err != nil {
+		return
 	} else {
 		err = this.Sign()
-		if err != nil {
-			fmt.Printf("签到失败: 账号: %s \n %s", this.Username, err)
-		} else {
-			fmt.Printf("签到成功: 账号: %s \n", this.Username)
-		}
+		return
 	}
+	return
 }
