d32760f9d77b67516dbe7d31c86a1e486da13b75
pa001024 <pa001024@qq.com>
2015-11-23 13:00:29 +0800

    update for vj v3

--- vjshi.go
+++ vjshi.go
@@ -21,6 +21,7 @@ import (
 
 const (
 	SITE_BASE        = "http://www.vjshi.com"
+	PASSPORT_BASE    = "http://passport.vjshi.com"
 	DEFAULT_PASSWORD = "vjscrapy99"
 	DEFAULT_UA       = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36"
 )
@@ -62,6 +63,9 @@ func main() {
 					break
 				}
 			}
+			if err != nil {
+				util.ERROR.Log(err)
+			}
 			key2user[uname] = UserState{uname, err == nil, bp}
 		}
 		count_fail, count_bp_all := 0, 0
@@ -92,14 +96,20 @@ func main() {
 		}
 		v := NewUser(*u, *p, *q)
 		if *isRegist {
-			err := v.Regist()
+			err := v.Register()
 			if err != nil {
 				fmt.Println(err)
 			} else {
 				fmt.Printf("注册成功: 账号: %s 密码: %s QQ: %s\n", *u, *p, *q)
 			}
 		} else {
-			v.LoginAndSign()
+			err := v.LoginAndSign()
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				bp, _ := v.GetBounsPoint()
+				fmt.Printf("签到成功: 账号: %s 当前积分: %d", *u, bp)
+			}
 		}
 	}
 }
@@ -118,29 +128,33 @@ func NewUser(username, pwd, qq string) (v *User) {
 }
 
 func (this *User) post(api string, param url.Values) (res *http.Response, err error) {
-	req, _ := http.NewRequest("POST", SITE_BASE+api, strings.NewReader(param.Encode()))
+	req, _ := http.NewRequest("POST", PASSPORT_BASE+api, strings.NewReader(param.Encode()))
 	req.Header.Set("User-Agent", DEFAULT_UA)
-	req.Header.Set("Referer", "http://www.vjshi.com/Passport/usercenter/")
+	req.Header.Set("Referer", "http://www.vjshi.com/")
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	return this.c.Do(req)
 }
 func (this *User) getCapture() (res *http.Response, err error) {
-	return this.get("/Base/verify/t/5122223123")
+	return this.get("/Base/verify/t/5122223123", "")
 }
-func (this *User) get(api string) (res *http.Response, err error) {
-	req, _ := http.NewRequest("GET", SITE_BASE+api, nil)
+func (this *User) get(api, base string) (res *http.Response, err error) {
+	if base == "" {
+		base = PASSPORT_BASE
+	}
+	req, _ := http.NewRequest("GET", base+api, nil)
 	req.Header.Set("User-Agent", DEFAULT_UA)
-	req.Header.Set("Referer", "http://www.vjshi.com/Passport/usercenter/")
+	req.Header.Set("Referer", "http://www.vjshi.com/")
 	return this.c.Do(req)
 }
-func (this *User) Regist() (err error) {
-	var err_msg = []string{"注册成功", "当前禁止注册新用户", "账号需要激活", "此用户名已被系统禁止注册", "用户名不合法", "包含不允许注册的词语", "用户名已经存在", "QQ格式有误", "Email不允许注册", "该Email已经被注册", "未定义错误", "", "验证码错误,请检查", "每个IP只能注册6个用户,请输入验证码注册", "", "验证码不能为空,请输入"}
+
+// unimpl
+func (this *User) Register() (err error) {
 	var verify = ""
 ret:
-	res, err := this.post("/Passport/doreg/", url.Values{
-		"userName": {this.Username},
+	res, err := this.post("/register/ajax", url.Values{
+		"username": {this.Username},
 		"password": {this.Password},
-		"email":    {this.QQ + "@qq.com"},
+		"email":    {this.QQ + "@qq.com"}, // 新版可以用任意邮箱注册
 		"verify":   {verify},
 	})
 	if err != nil {
@@ -148,13 +162,10 @@ ret:
 	}
 	rst, _ := ioutil.ReadAll(res.Body)
 	res.Body.Close()
-	var dst map[string]string
-	json.Unmarshal(rst, &dst)
-	code, _ := strconv.Atoi(dst["code"])
-	if code == 2 {
-		return errors.New(err_msg[code] + " /Passport/activeuser/auth/" + dst["auth"])
-	}
-	if code == 12 || code == 15 {
+	var obj map[string]string
+	json.Unmarshal(rst, &obj)
+	status := obj["status"]
+	if status == "error" {
 		res, err = this.getCapture()
 		ascgen.ShowFile(os.Stdout, res.Body, ascgen.Console{6, 14, 120}, true, true)
 		res.Body.Close()
@@ -166,62 +177,55 @@ ret:
 		}
 		goto ret
 	}
-	if code != 0 {
-		return errors.New(err_msg[code])
-	}
-	return
+	return errors.New(obj["message"])
 }
 
 func (this *User) Login() (err error) {
-	var err_msg = []string{"登录成功", "账号需要激活", "用户名不存在", "用户名或密码错误", "未知错误", "你的帐户已被禁用", "", "", "连续登陆错误3次，请稍后再试", "验证码错误,请检查"}
-
-	res, err := this.post("/Passport/dologin_ajax/", url.Values{
-		"userName": {this.Username},
+	res, err := this.get("/login/ajax?"+(url.Values{
+		"callback": {"jQuery18309862185863312334_" + util.JsCurrentTime()},
+		"username": {this.Username},
 		"password": {this.Password},
 		"verify":   {},
-	})
-
+	}).Encode(), "")
 	rst, _ := ioutil.ReadAll(res.Body)
 	res.Body.Close()
-	var dst map[string]string
-	json.Unmarshal(rst, &dst)
-	i, _ := strconv.Atoi(dst["code"])
-	if i == 10 {
-		return errors.New(dst["info"])
-	}
-	if i == 1002 {
-		return errors.New("账号审核中")
-	}
-	if i != 0 {
-		return errors.New(err_msg[i])
+	var obj map[string]string
+	json.Unmarshal(rst[41:len(rst)-1], &obj)
+	if obj["status"] != "success" {
+		return errors.New(obj["message"])
 	}
 	return
 }
 
 func (this *User) Sign() (err error) {
-	res, err := this.post("/Passport/daysign/", url.Values{})
+	res, err := this.get("/user/daysign?callback=jQuery18300392456746451165_"+util.JsCurrentTime(), "")
+	if err != nil {
+		return
+	}
 	rst, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return
 	}
 	res.Body.Close()
 	var obj map[string]string
-	json.Unmarshal(rst, &obj)
-	if obj["code"] == "success" {
-		return
-	} else {
-		return errors.New(obj["info"])
+	json.Unmarshal(rst[41:len(rst)-1], &obj)
+	if obj["status"] != "success" {
+		return errors.New(obj["message"])
 	}
+	return
 }
 
 func (this *User) GetBounsPoint() (v int, err error) {
-	res, err := this.get("/Passport/show_headerinfo/")
+	res, err := this.get("/User/", SITE_BASE)
+	if err != nil {
+		return
+	}
 	rst, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return
 	}
 	res.Body.Close()
-	ex := regexp.MustCompile(`积分：</span><span class="txt">(\d+)</span>`)
+	ex := regexp.MustCompile(`积分：(\d+)</p>`)
 	ret := ex.FindStringSubmatch(string(rst))
 	if len(ret) > 1 {
 		return strconv.Atoi(ret[1])
